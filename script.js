1e7bf56cb19017e83a273e669a8f31959e96305c
dzadzama <106168094+dzadzama@users.noreply.github.com>
2022-11-21 23:30:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+1.
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+//   };
+// console.log(user.studentstatus); 
+
+2.
+
+//let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+ //for (let x=0; x < numbers.length; x++);
+ //console.log(numbers);  
+
+
+ //let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+ // let x = 0
+// while(x < numbers.length) {
+//     console.log(numbers[x]);
+//     x++;
+// }
+
+3.
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for(let item of numbers) {
+//     if (item > 5) {
+//         console.log(item);
+//     }
+// }
+
+4.
+
+
+// let user = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentstatus: 'active'
+// }
+
+// if (user.age < 18 && user.studentstatus === 'active') {
+//     console.log('hello');
+// } else if(user.name === 'levani') {
+//     console.log('hello levani');
+// } else if(user.studentstatus === 'active' || user.age <25){
+//     console.log('hello world')
+// } else {
+//     console.log('error');
+// }
+
+5.
+
+// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+ //for (let nivti of array ){
+  //  if (typeof nivti=="string"){
+   //      console.log(nivti)
+    // }
+
+ //}
+
+
+ 6.
+// let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ];
+
+// for (let i=0; i < array.length; i++){
+//     let numbers = array[i];
+//     for(let x=0; x < numbers.length; x++){
+//         if(numbers[x] > 0){
+//             console.log(numbers[x]);
+//         }
+//     }
+// }
+
+
+7.
+
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+
+//   for (let y of array) {
+//      if (y % 2 != 1) {
+//        console.log(y);
+//      }
+//   }
+
+//    for (let y of array) {
+//       if (y % 2 != 0) {
+//         console.log(y);
+//    }
+// }
+
+
+
+8.
+
+// let users = [
+//     {username: 'giorgi', status: false},
+//     {username: 'levani', status: false},
+//     {username: 'anna', status: true}
+// ]
+
+// for(let y of users){
+//     if(y.status == true){
+//         console.log(y);
+//     }
+// }
+
+
